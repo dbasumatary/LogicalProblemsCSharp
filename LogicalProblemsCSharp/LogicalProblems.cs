d292661cd542be4b9aac3b064190f6dba80176e9
dbasumatary <basudhondip@gmail.com>
2023-06-07 12:56:21 +0530

    Added: Solutions for finding Perfect Number

--- LogicalProblemsCSharp/LogicalProblems.cs
+++ LogicalProblemsCSharp/LogicalProblems.cs
@@ -26,5 +26,32 @@ namespace LogicalProblemsCSharp
                 secondNum = nextNum;
             }
         }
+
+        //UC2 - Perfect Number
+        public void PerfectNumber()
+        {
+            Console.Write("Enter the number: ");
+            string userInput = Console.ReadLine();
+            int number = Convert.ToInt32(userInput);
+
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+            {
+                //Checking perfectly divisible numbers
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+
+            if (sum == number)
+            {
+                Console.WriteLine(number + " is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine(number + " is NOT a perfect number");
+            }
+        }
     }
 }
--- LogicalProblemsCSharp/Program.cs
+++ LogicalProblemsCSharp/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Logical Problems\n");
             LogicalProblems logicalProblems = new LogicalProblems();
             logicalProblems.FibonacciSeries();
+            logicalProblems.PerfectNumber();
         }
     }
 }
