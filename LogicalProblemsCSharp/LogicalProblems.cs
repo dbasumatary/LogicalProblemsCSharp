1fdac55367de1662027eac35fe511146eaed6a8c
dbasumatary <basudhondip@gmail.com>
2023-06-07 12:49:33 +0530

    Added: Solutions for Fibonacci Series

---
+++ LogicalProblemsCSharp/LogicalProblems.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblemsCSharp
+{
+    public class LogicalProblems
+    {
+        //UC1 - Fibonacci Series
+        public void FibonacciSeries()
+        {
+            int firstNum = 0, secondNum = 1;
+            Console.Write("Enter the number up to which to see the Fibonacci series: ");
+            string userInput = Console.ReadLine();
+            int count = Convert.ToInt32(userInput);
+
+            Console.Write("The Fibonacci series is : " + firstNum + " " + secondNum);
+
+            for (int i = 2; i < count; i++)
+            {
+                int nextNum = firstNum + secondNum;
+                Console.Write(" " + nextNum);
+                firstNum = secondNum;
+                secondNum = nextNum;
+            }
+        }
+    }
+}
--- LogicalProblemsCSharp/Program.cs
+++ LogicalProblemsCSharp/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Logical Problems\n");
+            LogicalProblems logicalProblems = new LogicalProblems();
+            logicalProblems.FibonacciSeries();
         }
     }
 }
